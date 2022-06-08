4b0aa80c7712b13d55faf36fe74b65a0d0586c22
DhananjayDhoke <dhananjaydhoke3@gmail.com>
2022-06-08 21:25:18 +0530

    pending

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./addTask.module.css";
 
-const AddTask = () => {
+const AddTask = ({getText}) => {
+
   // NOTE: do not delete `data-testid` key value pair
+     let [text,setText] =useState()
+
+
   return (
     <div className={styles.todoForm}>
-      <input data-testid="add-task-input" type="text" />
-      <button data-testid="add-task-button"></button>
+      <input data-testid="add-task-input" type="text" className="inp" onChange={(e)=>{
+        setText(e.target.value)
+      }}/>
+      <button data-testid="add-task-button" className="but" onClick={()=>{getText(text)}} >+</button>
     </div>
   );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import AddTask from "../AddTask/AddTask";
 import styles from "./task.module.css";
 
 const Task = () => {
   // NOTE: do not delete `data-testid` key value pair
+
+  const [showText, setShowtext] = useState()
+
+  const [count,setCount] = useState(0)
+
+  let handelCounter =(data)=>{
+    
+    if(count>=0){
+      setCount(count+value)
+
+    }
+
+  }
+    
+  const getdata=(value)=>{
+    // const payload={
+    //      id:1,
+    //     text:value,
+    //     done:false,
+    //     count:0
+    // }
+
+    setShowtext(value)
+ }
+    
   return (
     <li data-testid="task" className={styles.task}>
+     <AddTask getText={getdata}></AddTask>
       <input type="checkbox" data-testid="task-checkbox" />
-      <div data-testid="task-text"></div>
+      <div data-testid="task-text">{showText}</div>
       {/* Counter here */}
+       <button onClick={()=>{handelCounter(1)}}>+</button>
+       <h3>{count}</h3>
+       <button onClick={()=>{handelCounter(-1)}}>-</button>
       <button data-testid="task-remove-button"></button>
     </li>
   );
